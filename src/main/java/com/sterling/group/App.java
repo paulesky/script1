ea2175118662d94048b5b023abc4d25d31579f26
paulesky <adediranpaul.ap@gmail.com>
2025-05-03 01:31:17 -0700

    second commit

---
+++ src/main/java/com/sterling/group/App.java
@@ -0,0 +1,10 @@
+package com.sterling.group;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World, How una dey, This is new one for me!");
+    }
+}
